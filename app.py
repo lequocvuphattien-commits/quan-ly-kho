d83baba06ac34df075507b004cb7c9314165309e
Le Quoc Vu <lequocvuphattien@gmail.com>
2026-05-23 16:27:33 +0700

    Update

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown("""
     }
     /* Loại bỏ khoảng cách thừa phía trên cùng của trang */
     #root > div:nth-child(1) > div > div > div > div > section > div {
-        padding-top: 10px !important;
+        padding-top: 12px !important;
     }
     </style>
 """, unsafe_allow_html=True)
