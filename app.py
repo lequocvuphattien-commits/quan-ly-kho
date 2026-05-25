f6af8ec482a03c7993c96a24d3840b7a669bd6f8
Le Quoc Vu <lequocvuphattien@gmail.com>
2026-05-25 23:37:53 +0700

    Update

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ st.markdown("""
     <style>
     .block-container { padding-top: 1rem !important; padding-bottom: 1rem !important; }
     div.stButton > button[kind="primary"] { background-color: #28a745 !important; color: white !important; }
-    h1 { padding-bottom: 0rem !important; margin-bottom: 0rem !important; }
-    h3 { padding-top: 0rem !important; margin-top: -2rem !important; }
+    h1 { padding-bottom: -2rem !important; margin-bottom: 0rem !important; }
+    h3 { padding-top: -2rem !important; margin-top: 0rem !important; }
     div[data-testid="stSelectbox"] { margin-bottom: -1rem !important; }
     
     /* Ép chữ Loại và 2 nút Nhập/Xuất nằm ngang hàng tuyệt đối trên mọi màn hình (Cả PC lẫn Mobile) */
