05ab9ba0a8d633f6087b62afab91f75fcb4d761c
Le Quoc Vu <lequocvuphattien@gmail.com>
2026-05-26 14:30:30 +0700

    Update

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ st.markdown("""
         display: flex !important; 
         flex-direction: row !important; 
         align-items: center !important; 
-        flex-wrap: nowrap !important; /* Cấm bẻ dòng */
+        flex-wrap: nowrap !important;
+        margin-bottom: -15px !important; /* Cấm bẻ dòng */
     }
     div[data-testid="stRadio"] > label { 
         margin-bottom: 0px !important; 
