d1ff41c1ab6837bb82d144def7da90992d5c41a6
Le Quoc Vu <lequocvuphattien@gmail.com>
2026-05-27 15:11:54 +0700

    Update

--- views/print_export_view.py
+++ views/print_export_view.py
@@ -91,16 +91,16 @@ def export_phieu_xuat_excel(export_data, selected_date):
     # --- GHI NGÀY IN PHIẾU (MERGE A6:F6) ---
     date_str = f"Ngày {selected_date.day:02d} tháng {selected_date.month:02d} năm {selected_date.year}"
     #ws.merge_cells('A6:F6')
-    ws['C6'] = date_str
-    ws['C6'].font = font_italic
-    ws['C6'].alignment = Alignment(horizontal="center", vertical="center")
+    ws['E16'] = date_str
+    ws['E16'].font = font_italic
+    ws['E16'].alignment = Alignment(horizontal="center", vertical="center")
     ws.row_dimensions[6].height = 20
        
     # Thiết lập độ rộng dòng tiêu đề bảng (Đẩy sang Dòng số 8)
     ws.row_dimensions[8].height = 26
     
-    headers = ["STT", "Tên hàng hóa", "Đvt", "Số Lượng", "Diễn Giải", "Ghi Chú"]
-    cols = ["A", "B", "C", "D", "E", "F"]
+    headers = ["STT", "Tên hàng hóa", "Đvt", "Số Lượng", "Ghi Chú"]
+    cols = ["A", "B", "C", "D", "E"]
     
     for col_letter, header_text in zip(cols, headers):
         cell = ws[f"{col_letter}8"]
@@ -131,8 +131,7 @@ def export_phieu_xuat_excel(export_data, selected_date):
         ws[f"C{current_row}"].alignment = Alignment(horizontal="center", vertical="center")
         ws[f"D{current_row}"].alignment = Alignment(horizontal="center", vertical="center")
         ws[f"D{current_row}"].number_format = '#,##0' 
-        ws[f"E{current_row}"].alignment = Alignment(horizontal="left", vertical="center", wrap_text=True)
-        ws[f"F{current_row}"].alignment = Alignment(horizontal="left", vertical="center")
+        ws[f"E{current_row}"].alignment = Alignment(horizontal="left", vertical="center")
         
         for col_letter in cols:
             cell = ws[f"{col_letter}{current_row}"]
